d0bbe744e172317d9648d0ca76b1bb64840e3bba
Andrew McGregor <me@andrewmcgregor.me>
2013-07-09 22:41:43 -0500

    updating callback

--- app/helpers/twitters.rb
+++ app/helpers/twitters.rb
@@ -1,5 +1,5 @@
 def request_token
-  callback_url = "http://127.0.0.1:9393/oauth/callback"
+  callback_url = "http://sleepy-island-2135.herokuapp.com/oauth/callback"
   consumer = OAuth::Consumer.new(ENV['TWITTER_KEY'],ENV['TWITTER_SECRET'], :site => "https://api.twitter.com")
   session[:request_token] = consumer.get_request_token(:oauth_callback => callback_url)
 end
